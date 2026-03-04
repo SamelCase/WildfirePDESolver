006ff79240366689c33d51755886d358f68bb0a8
SamelCase <samul.geary@gmail.com>
2026-03-04 00:35:01 -0700

    Reproduced fig 2.3 with SciPy solver

--- Figure2.3.py
+++ Figure2.3.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+from time import perf_counter
 
 """
 This is an exploratory file trying to empirically reproduce figure 2.3
@@ -12,10 +13,13 @@ work to improve on it by predicting the matrix A
 def fig23():
     # Time interval
     t_span = (0, 10)
-    t_eval = np.linspace(0, 10, 1000)
+    t_eval = np.linspace(0, 10, 10000000)
 
-    # Initial amplitudes
+    # Initial amplitudes eyeballed from the textbook figure.
     amplitudes = [0, 0.3, 1, 2]
+    tLin, tNLin = [], []
+    # SciPy's solve_ivp expects the system to be in first-order form, so we need to rewrite our second-order ODE as 
+    # a system of two first-order ODEs. We define theta as the angle and omega as the angular velocity (theta').
 
     # Linear system
     def linear(t, y):
@@ -28,29 +32,45 @@ def fig23():
         return [omega, -np.sin(theta)]
 
     # Create figure
-    fig, axs = plt.subplots(2, 1, figsize=(8, 8))
+    fig, axs = plt.subplots(2, 1, figsize=(10, 8))
 
-    # --- (b) Linear ---
+    # Linear approximation, where for small theta we can say sin(theta) is approximately equal to theta
     for A in amplitudes:
+        start = perf_counter()
         sol = solve_ivp(linear, t_span, [A, 0], t_eval=t_eval)
+        end = perf_counter()
+        tLin.append(end - start)
         axs[0].plot(sol.t, sol.y[0])
 
     axs[0].set_title("Linear: θ'' + θ = 0")
     axs[0].set_ylabel("θ(t)")
+    axs[0].set_xlim(0,10)
+    axs[0].set_ylim(-2,2)
     axs[0].grid()
 
-    # --- (c) Nonlinear ---
+    # True nonlinear solutions, more expensive to compute
     for A in amplitudes:
+        start = perf_counter()
         sol = solve_ivp(nonlinear, t_span, [A, 0], t_eval=t_eval)
+        end = perf_counter()
+        tNLin.append(end - start)
         axs[1].plot(sol.t, sol.y[0])
 
     axs[1].set_title("Nonlinear: θ'' + sin(θ) = 0")
     axs[1].set_xlabel("Time")
     axs[1].set_ylabel("θ(t)")
+    axs[1].set_xlim(0,10)
+    axs[1].set_ylim(-2,2)
     axs[1].grid()
 
     plt.tight_layout()
     plt.savefig("figure2.3.png")
+    print(f"Mean Linear time: {np.mean(tLin):.4f} seconds")
+    print(f"Mean Nonlinear time: {np.mean(tNLin):.4f} seconds")
+
+    # We saw that in such a simple case, the two algorithms are actually pretty comparable in terms of runtime, 
+    # which is interesting. We will see if we can do better by predicting the matrix A in the future.
+
 
 if __name__ == "__main__":
     fig23()
